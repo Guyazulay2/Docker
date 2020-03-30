0c5262a4191d597d210a02d83d41ff5890305343
Guyazulay2 <59178662+Guyazulay2@users.noreply.github.com>
2020-03-30 05:15:31 +0200

    Add files via upload

---
+++ docker.py
@@ -0,0 +1,111 @@
+#!/bin/python3.7
+
+from time import sleep
+import os
+import datetime
+
+def Install():
+    print("<<<Install Docker>>>")
+    sleep(0.5)
+    os.system("curl - fsSL https: // get.docker.com - o get - docker.sh")
+    os.system("bash get - docker.sh")
+    print("<<<Docker Installation is Done>>>")
+
+
+def Pull_Images():
+    s = input(">>Enter the image name that you want to pull >>:")
+    os.system("sudo docker pull" " " + s)
+    sleep(0.5)
+    print("Your Image :",s ,"Pulling Successfully")
+    sleep(0.5)
+    print("The Images :")
+    os.system("sudo docker images")
+
+
+def Deploy():
+        print("Those the images :")
+        sleep(0.5)
+        os.system("sudo docker images")
+        b = input("Enter the image that you want to deploy >>:")
+        cont = input("How many Containers Would you like to create :")
+        for i in cont:
+            os.system("sudo docker run -d" " " + b)	
+        os.system("sudo docker ps -a")
+
+
+def show():
+    print("---Showing All Containers And Images---")
+    os.system("sudo docker images")
+    os.system("sudo docker ps -a")
+
+
+def Delete():
+        print(">>> Those the containers <<<")
+        os.system("sudo docker ps -a")
+        choose = input("(1)>>Remove Container\n(2)>>Remove all containers\n(3)>>Remove Images\nYour choice >>:")
+        if choose == "1":
+            a = input("(1)>>Enter the container that you want to delete >>:")
+            os.system("sudo docker stop" " " + a)
+            os.system("sudo docker rm" " " + a)
+            sleep(0.5)
+            print("Container >>:" ,a , "Deleted Successfully")
+
+
+        elif choose == "2":
+            s = input("You sure that you want to delete all containers ? (y/n) >>:")
+            if s == "y" or s == "yes":
+                print("Delete all containers ..")
+                sleep(0.5)
+                print("Stop the containers..")
+                sleep(0.5)
+                os.system("docker stop $(docker ps -a -q)")
+                print("Delete the containers..")
+                sleep(0.5)
+                os.system("docker rm $(docker ps -a -q)")
+            elif s == "n" or s == "no":
+                print("Bye Bye")
+
+
+        elif choose == "3":
+            os.system("sudo docker images")
+            image = input("Enetr the Image id to delete >>:")
+            print("Deleting >>:", image)
+            sleep(0.5)
+            os.system("sudo docker rmi" " " + image)
+            print(image,"<<Deleted Successfully>>")
+            os.system("sudo docker images")
+        else:
+             print("Enter 1 - 3 only !")
+
+def main():
+    os.system("clear")
+    y = datetime.datetime.now()
+    x = datetime.date.today()
+    print("------Date :" ,x)
+    print("------Time Now :",y.strftime("%R"))
+    os.system("docker --version")
+    sleep(0.5)
+    while True:
+        choice = input("    ------Menu------\n(1)>>Install Docker\n(2)>>Pull Images\n(3)>>Deploy Images\n(4)>>Show all images and Containers\n(5)>>Delete Containers And images\n(6)>>Exit\n  Your Choise >>:")
+        if choice == "1":
+            Install()
+        if choice == "2":
+            Pull_Images()
+        if choice == "3":
+            Deploy()
+        if choice == "4":
+            show()
+        if choice == "5":
+            Delete()
+        if choice == "6":
+            print("Bye Bye")
+            break
+        else:
+            print("Enter 1 - 6 only !")
+
+main()
+
+
+
+
+
